5c6f7701cb6790776821e32ee34dca8106387e76
Andrew Giang <giang184@gmail.com>
2021-09-10 09:57:03 -0700

    add a nav bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Navbar(){
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="">TapBrewBooHoo</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDark" aria-controls="navbarDark" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse show" id="navbarDark">
+          <ul className="navbar-nav me-auto mb-2 mb-xl-0">
+          </ul>
+          <ul className="nav navbar-nav float-md-right">
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Not implemented yet" aria-label="Search"></input>
+              <button className="btn btn-outline-light" type="submit">Search</button>
+            </form>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
