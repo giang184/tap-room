7010a6fda386895a559e85669360a22f3e61d8aa
Andrew Giang <giang184@gmail.com>
2021-09-10 10:08:14 -0700

    add new tap form

---
+++ src/components/NewTapForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { v4 } from 'uuid';
+import PropTypes from 'prop-types';
+import ReusableForm from "./ReusableForm";
+
+function NewTapForm(props){
+  function handleNewTapFromSubmission(e) {
+    e.preventDefault();
+    props.onNewTapCreation({
+      name: e.target.name.value,
+      description: e.target.description.value,
+      quantity: e.target.quantity.value,
+      id: v4()
+    });
+  }
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewTapFromSubmission}
+        buttonText="Add"
+      />
+    </React.Fragment>
+  );
+}
+
+
+NewTapForm.propTypes = {
+  onNewTapCreation: PropTypes.func
+}
+
+export default NewTapForm;
