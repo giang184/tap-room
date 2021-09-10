64418be6518cbe24a432a91be894b5f4b4b0c616
Andrew Giang <giang184@gmail.com>
2021-09-10 11:37:41 -0700

    add edit tap file

---
+++ src/components/EditTapForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+function EditTapForm (props) {
+  const { tap } = props;
+
+  function handleEditTapFormSubmission(event) {
+    event.preventDefault();
+    props.onEditTap({
+      name: event.target.name.value, 
+      description: event.target.description.value, quantity: event.target.quantity.value, 
+      id: tap.id
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler={handleEditTapFormSubmission}
+        tap={tap}
+        buttonText="Update Tap" />
+    </React.Fragment>
+  );
+}
+
+EditTapForm.propTypes = {
+  tap: PropTypes.object,
+  onEditTap: PropTypes.func
+};
+
+export default EditTapForm;
